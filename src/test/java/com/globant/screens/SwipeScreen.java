3eba1f38fc2308ee158d371ec219396125975b42
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-03-05 22:40:12 -0500

    horizontal swipe ready

--- src/test/java/com/globant/screens/SwipeScreen.java
+++ src/test/java/com/globant/screens/SwipeScreen.java
@@ -3,15 +3,27 @@ package com.globant.screens;
 import com.globant.utils.screens.BaseScreen;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AndroidFindBy;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.PointerInput;
+import org.openqa.selenium.interactions.Sequence;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.NoSuchElementException;
 
 public class SwipeScreen extends BaseScreen {
+    private static final int TOTAL_CARDS_AMOUNT = 6;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"Swipe horizontal\")")
     private WebElement header;
 
     @AndroidFindBy(uiAutomator = "new UiSelector().text(\"FULLY OPEN SOURCE\")")
     private WebElement firstCardTitle;
+
+    @AndroidFindBy(accessibility = "Carousel")
+    private WebElement carousel;
 
     public SwipeScreen(AndroidDriver androidDriver) {
         super(androidDriver);
@@ -22,11 +34,40 @@ public class SwipeScreen extends BaseScreen {
     }
 
     public boolean isFirstCardTitleVisible() {
-        return isElementVisible(firstCardTitle);
+        try {
+            return isElementVisible(firstCardTitle);
+        } catch (NoSuchElementException | TimeoutException e){
+            return false;
+        }
     }
+
 
     public boolean areElementsVisible() {
         return isHeaderVisible() && isFirstCardTitleVisible();
+    }
+
+    public void swipeRight() {
+        Dimension size = androidDriver.manage().window().getSize();
+        // Define the start and end coordinates for the swipe
+        int startX = 1180; // Start from the right side of the screen
+        int startY = 1568; // Y coordinate doesn't change for horizontal swipe
+        int endX = 360;   // End at the left side of the screen
+        int endY = 500;   // Y coordinate doesn't change for horizontal swipe
+
+        PointerInput pointerInput = new PointerInput(PointerInput.Kind.TOUCH, "swipe");
+        Sequence sequence = new Sequence(pointerInput, 1)
+                .addAction(pointerInput.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startX, startY))
+                .addAction(pointerInput.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+                .addAction(pointerInput.createPointerMove(Duration.ofMillis(200), PointerInput.Origin.viewport(), endX, endY))
+                .addAction(pointerInput.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+        androidDriver.perform(Collections.singletonList(sequence));
+    }
+
+    public void swipeToTheLastCard(int swipedCardsAmount) {
+        for(int i = 0; i < TOTAL_CARDS_AMOUNT - 1 -swipedCardsAmount; i++) {
+            swipeRight();
+        }
     }
 
 }
--- src/test/java/com/globant/tests/SwipeCardsTest.java
+++ src/test/java/com/globant/tests/SwipeCardsTest.java
@@ -1,6 +1,22 @@
 package com.globant.tests;
 
+import com.globant.screens.HomeScreen;
+import com.globant.screens.SwipeScreen;
 import com.globant.utils.tests.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class SwipeCardsTest extends BaseTest {
+
+    @Test
+    public void swipeCardsOnTheSwipeSectionTest() {
+        HomeScreen homeScreen = getHomeScreen();
+        SwipeScreen swipeScreen = homeScreen.clickOnSwipeOption();
+
+        Assert.assertTrue(swipeScreen.isFirstCardTitleVisible());
+        swipeScreen.swipeRight();
+        Assert.assertFalse(swipeScreen.isFirstCardTitleVisible());
+
+        swipeScreen.swipeToTheLastCard(1);
+    }
 }
